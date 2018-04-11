d65d3478e53a00ebe483d5939a0b4a170dfa4106
github username <github email address>
2018-04-11 11:30:09 -0400

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,38 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
+
+
 class BotCollection extends React.Component {
   //your code here
+  constructor(props){
+    super(props);
+      this.state = {
+        clicked: false
+    }
+  }
+
+  renderBotCards = () => {
+      return this.props.props.map(bot => {
+        return <BotCard clickFunction={ this.handleClick } key={ bot.id } bot={ bot } />
+    })
+  }
+
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    }, () => {
+      console.log(this.state.clicked)
+    })
+  }
 
   render(){
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          { this.renderBotCards() }
     		</div>
   	  </div>
   	);
