0cbd74413bc71423beb9d8fcc663614787ab7bc2
rfarney3 <farneyr@yahoo.com>
2018-04-11 11:30:40 -0400

    ryan-done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,52 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs"
+import Filter from "../components/Filter"
 
 class BotCollection extends React.Component {
-  //your code here
+  constructor() {
+    super()
+
+    this.state = {
+      bot: "",
+      query: "",
+      filteredBots: []
+    }
+  }
+
+  addYour = (b) => {
+    this.setState({
+      bot: b
+    })
+  }
+
+  close = () => {
+    this.setState({
+      bot:""
+    })
+  }
+
+  handleFilter = (event) => {
+    this.setState({
+      query: event.target.value.toLowerCase()
+    }, () => {
+      this.setState({
+        filteredBots: this.props.allBots.filter(bot => {return bot.name.toLowerCase().includes(this.state.query)})
+      }, () => console.log(this.state.filteredBots))
+    })
+  }
+
 
   render(){
+    const botArr = this.props.allBots.map(bot => {return <BotCard props={bot} addYour={this.addYour}/>})
+    const filteredBotArr = this.state.filteredBots.map(bot => {return <BotCard props={bot} addYour={this.addYour}/>})
+
   	return (
   	  <div className="ui four column grid">
+        <Filter onChange={this.handleFilter} query={this.state.query}/>
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {this.state.bot !== "" ? <BotSpecs show={this.state.bot} close={this.close} addYourArmy={this.props.addYour}/> : null}
+    		  {filteredBotArr.length > 0 ? filteredBotArr : botArr}
     		</div>
   	  </div>
   	);
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,19 +4,16 @@ import BotCard from "../components/BotCard";
 class YourBotArmy extends React.Component {
   //your bot army code here...
 
-  render(){
-    return (
-      <div className="ui segment inverted olive bot-army">
-        <div className="ui five column grid">
-          <div className="row bot-army-row">
-            {/*...and here...*/}
-            Your Bot Army
+  render() {
+      return (
+        <div className="ui segment inverted olive bot-army">
+          <div className="ui five column grid">
+            <div className="row bot-army-row">
+              {this.props.yourArmy.map(bot => {return <BotCard props={bot} addYour={this.props.remove}/>})}
+            </div>
           </div>
-        </div>
-      </div>
-    );
-  }
-  
+        </div>);
+    }
 };
 
 export default YourBotArmy;
