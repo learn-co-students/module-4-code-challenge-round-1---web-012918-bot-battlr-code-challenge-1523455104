bc495d8ae7175ca691a98a5ef5efa47f1d86dee7
allinthemind66 <r.nilon92@gmail.com>
2018-04-11 11:29:07 -0400

    bad commit

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,14 +2,28 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
+  constructor(props){
+    super(props)
+  
+  }
+
+  // renderBotCard = () => {
+  //   return this.state.bots.map(bot => {
+  //     // console.log(this.props.addBotToMyArmy)
+  //     return <BotCard addBotToMyArmy={this.props.addBotToMyArmy} props={bot}/>
+  //   })
+  // }
+
+
 
   render(){
+    // debugger
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
     		  Collection of all bots
+          {this.props.renderBotCard()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,53 @@
 import React from "react";
-
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+import BotCard from '../components/BotCard'
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor(){
+    super()
+    this.state = {
+      myBots: [],
+      botsCollection: []
+    }
+  }
 
+  componentDidMount = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(resp => resp.json())
+    .then(json => this.setState({
+      botsCollection: json
+    }, () => console.log(this.state.botsCollection)))
+  }
+
+  renderBotCard = () => {
+
+    return this.state.botsCollection.map(bot => {
+      // console.log(this.props.addBotToMyArmy)
+      return <BotCard addBotToMyArmy={this.addBotToMyArmy} props={bot}/>
+    })
+  }
+
+  addBotToMyArmy = (e) => {
+    // console.log(e.target.parentElement.parentElement.dataset.id)
+    let bot = this.state.botsCollection.find((bot) => {
+      bot.id === e.target.parentElement.parentElement.dataset.id
+    })
+    console.log(bot)
+//NEED TO FIND BOT FROM BOTSCOLLECTION AND PUSH INTO MYBOTS, THEN RENDER MYBOTS
+    e.target.parentElement.parentElement.remove()
+    this.setState({
+      myBots: [...this.state.myBots, bot]
+
+    })
+
+  }
   render() {
+    console.log(this.state.bots)
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection renderBotCard={this.renderBotCard} bots={this.state.botsCollection}/>
+        <YourBotArmy props={this.state}/>
       </div>
     );
   }
