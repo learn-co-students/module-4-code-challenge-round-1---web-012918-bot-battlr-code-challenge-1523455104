06db90c95491b5c0efda85d29805e285d3777823
github username <github email address>
2018-04-11 11:25:10 -0400

    omg

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,39 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import YourBotArmy from "./YourBotArmy"
 
 class BotCollection extends React.Component {
-  //your code here
+  constructor(){
+    super()
+
+    this.state = {
+      clicked: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
 
   render(){
   	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+  	  <div>
+    		<div key={this.props.id}>
+          <img src={this.props.data["avatar_url"]}></img>
+          <br></br>
+          <h1>{this.props.data.name}</h1>
+          <br></br>
+          Health: <p>{this.props.data.health}</p>
+          <br></br>
+          Damage: <p>{this.props.data.damage}</p>
+          <br></br>
+          Armor: <p>{this.props.data.armor}</p>
+          <br></br>
+          Class: <p>{this.props.data["bot_class"]}</p>
+          <br></br>
+          Catchphrase: <p>{this.props.data.catchphrase}</p>
+          <br></br>
+          <button onClick={this.handleClick}>Enlist Me!</button>
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,28 @@
 import React from "react";
+import BotCard from "../components/BotCard.js"
+import BotCollection from "./BotCollection.js"
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(res => res.json())
+      .then(json => this.setState({data: json}))
+  }
+
+  createBotCollection = () => {
+    return this.state.data.map(robot => <BotCollection data={robot}/>)
+  }
 
   render() {
+
     return (
       <div>
-        {/* put your components here */}
+        {this.createBotCollection()}
       </div>
     );
   }
