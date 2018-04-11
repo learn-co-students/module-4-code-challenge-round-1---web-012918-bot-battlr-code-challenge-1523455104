af692e946e9b00886a5770c51a105b8f27939dbe
alin684 <al684@cornell.edu>
2018-04-11 11:28:18 -0400

    testing

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,41 @@
 import React from "react";
+import BotCollection from './BotCollection.js'
+import YourBotArmy from './YourBotArmy.js'
+
+const URL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+
+    this.state = {
+      botArray: [],
+      yourArmy: []
+    }
+  }
+  componentDidMount = () => {
+    fetch(URL)
+    .then(res => res.json())
+    .then(json => this.setState({
+      botArray: json
+    }))
+  }
+
+  addToArmy = (bot) => {
+    if (!this.state.yourArmy.includes(bot)) {
+      this.setState({
+        yourArmy: [...this.state.yourArmy, bot]
+      })
+    }
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy army={this.state.yourArmy} />
+        <BotCollection bots={this.state.botArray} addToArmy={this.addToArmy} />
+
       </div>
     );
   }
