6e0004156d1ec1be3264253d421f29ff642f0dea
scott <you@example.comitsmescottb123@gmail.com>
2018-04-11 11:33:19 -0400

    done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,40 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from '../components/BotSpecs'
 
 class BotCollection extends React.Component {
-  //your code here
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      bots: this.props.bots,
+      clicked: false
+    }
+  }
+
+
+  renderAllBots = () => {
+    return this.props.bots.map(bot => {
+      return <BotCard handleClick={this.props.handleClick} key={bot.id} bot={bot} value='not clicked'/>
+    })
+  }
+
+  // handleClick = (event) => {
+  //   this.setState({
+  //     clicked: !this.state.clicked
+  //   })
+  // }
+
+
+
+
 
   render(){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.renderAllBots()}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,35 @@
 import React from "react";
+import YourBotArmy from './YourBotArmy'
+import BotCollection from './BotCollection'
+
+const URL = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+      clicked: false
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(URL).then(resp => resp.json()).then(json => this.setState({bots: json}))
+  }
+
+  handleClick = (event) => {
+    console.log(event)
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy bots={this.state.bots}/>
+        <BotCollection bots={this.state.bots} handleClick={this.handleClick} />
       </div>
     );
   }
